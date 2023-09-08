9c617506a017e4738fa38104bddc17853060a7eb
Cedo-CP <102039597+Cedo-CP@users.noreply.github.com>
2023-09-08 14:19:22 +0000

    morning 9-8

--- financial-summarizer/src/App.js
+++ financial-summarizer/src/App.js
@@ -8,7 +8,7 @@ function App() {
     const [pdfUrls, setPdfUrls] = useState('');
     const [status, setStatus] = useState({});
     const [summarizedResponses, setSummarizedResponses] = useState({});
-    const FLASK_SERVER_URL = 'http://127.0.0.1:5000';
+    const FLASK_SERVER_URL = 'literate-journey-r5v477rjr9q2rj7-5000.app.github.dev';
 
     const handleIdentifyDocuments = async (event) => {
         event.preventDefault();
